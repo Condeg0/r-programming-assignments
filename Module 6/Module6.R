b236b5267197bb5ffa66ca73a1b56be663a5c3a3
Condeg0 <118837512+Condeg0@users.noreply.github.com>
2025-10-02 10:08:48 -0400

    Create Module6.R

---
+++ Module 6/Module6.R
@@ -0,0 +1,14 @@
+# Define matrices
+A <- matrix(c(2, 0, 1, 3), ncol = 2)
+B <- matrix(c(5, 2, 4, -1), ncol = 2)
+
+# Addition
+A_plus_B <- A + B
+
+# Subtraction
+A_minus_B <- A - B
+
+A
+B
+A_plus_B
+A_minus_B
